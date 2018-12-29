fa99cd2bed1fdbe385f5c278564e980f566df278
Stephen Collins <stephenc211@gmail.com>
2018-12-28 21:36:48 -0500

    added WebWorker mock to App tests

--- src/App/index.test.tsx
+++ src/App/index.test.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './index';
 
+jest.mock('./WebWorker')
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
